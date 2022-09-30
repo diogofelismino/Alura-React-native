a04746c67ddf25305f9edfd4d5b75fa29b5af936
Diogo Felismino da Silva <diogo.silva@notacontrol.matriz>
2022-09-30 15:00:46 -0400

    Teste

--- src/telas/produtor/index.js
+++ src/telas/produtor/index.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, StyleSheet, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 export default function Produtor(){
 
     const route = useRoute();
+    const [nome, imagem ,cestas] = route.params;
     
 
     return(
-        <Text>{route.params.nome}</Text>
+        <FlatList
+            data={cestas}
+            renderItem={({item}) => 'a'}/>
     )
 }
+
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+
+        marginTop: -23,
+
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
